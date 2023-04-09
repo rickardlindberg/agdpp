cb5d3ecd6ee6f823658fc4d3842d23456c410f95
Rickard Lindberg <rickard@rickardlindberg.me>
2023-04-09 07:41:11 +0200

    Structure in place for Game and GameLoop.

--- agdpp.py
+++ agdpp.py
@@ -1,4 +1,30 @@
 #!/usr/bin/env python3
 
+class Game:
+
+    """
+    I draw an animated circle until the user closes the window.
+
+    >>> game = Game(GameLoop())
+    >>> game.run()
+    DRAW_CIRCLE
+    EXIT
+    """
+
+    def __init__(self, loop):
+        self.loop = loop
+
+    def run(self):
+        self.loop.run(self)
+
+    def tick(self):
+        print("DRAW_CIRCLE")
+
+class GameLoop:
+
+    def run(self, game):
+        game.tick()
+        print("EXIT")
+
 if __name__ == "__main__":
-    print("Hello from agdpp!")
+    Game().run()
