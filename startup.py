aadd1a2a025fab5b86131e077f89f72de2f6281b
Rickard Lindberg <rickard@rickardlindberg.me>
2023-08-30 07:02:40 +0200

    Add startup entry point and have it loop.

--- startup.py
+++ startup.py
@@ -5,13 +5,31 @@ class StartupApplication:
 
     """
     I draw an application select screen:
-
-    >>> events = StartupApplication.run_in_test_mode(
+    >>> StartupApplication.run_in_test_mode(
     ...     events=[
     ...         [GameLoop.create_event_user_closed_window()],
     ...     ]
     ... )
+    GAMELOOP_INIT =>
+        resolution: (1280, 720)
+        fps: 60
+    GAMELOOP_QUIT =>
+    GAMELOOP_INIT =>
+        resolution: (1280, 720)
+        fps: 60
+    GAMELOOP_QUIT =>
     """
+
+    @staticmethod
+    def create():
+        """
+        >>> isinstance(StartupApplication.create(), StartupApplication)
+        True
+        """
+        return StartupApplication(
+            loop=GameLoop.create(),
+            loop_condition=InifiteLoopCondition()
+        )
 
     @staticmethod
     def run_in_test_mode(events=[]):
@@ -21,14 +39,19 @@ class StartupApplication:
             ]
         )
         events = loop.track_events()
-        StartupApplication(loop).run()
+        StartupApplication(
+            loop=loop,
+            loop_condition=FiniteLoopCondition(2)
+        ).run()
         return events
 
-    def __init__(self, loop):
+    def __init__(self, loop, loop_condition):
         self.loop = loop
+        self.loop_condition = loop_condition
 
     def run(self):
-        self.loop.run(self)
+        while self.loop_condition.active():
+            self.loop.run(self)
 
     def event(self, event):
         if event.is_user_closed_window():
@@ -36,3 +59,35 @@ class StartupApplication:
 
     def tick(self, dt):
         pass
+
+class InifiteLoopCondition:
+
+    def active(self):
+        """
+        >>> InifiteLoopCondition().active()
+        True
+        """
+        return True
+
+class FiniteLoopCondition:
+
+    def __init__(self, iterations):
+        self.iterations = iterations
+        self.count = 0
+
+    def active(self):
+        """
+        >>> condition = FiniteLoopCondition(iterations=2)
+        >>> condition.active()
+        True
+        >>> condition.active()
+        True
+        >>> condition.active()
+        False
+        """
+        flag = self.count < self.iterations
+        self.count += 1
+        return flag
+
+if __name__ == "__main__":
+    StartupApplication.create().run()
